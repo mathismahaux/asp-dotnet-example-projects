bd30ca9d6c7c4931d2ff3b9ea274ec347301b040
Mathis <mathis.mahaux@gmail.com>
2025-02-18 17:58:24 +0100

    Add SpaceExpedition.cs

---
+++ projects/SpaceExpedition.cs
@@ -0,0 +1,26 @@
+using System;
+
+public class SpaceExpedition
+{
+  public static void Main(string[] args){
+    // Your code goes here
+    bool isAtmosphereBreathable  = true;
+    bool isGravityStable  = false;
+    bool resourcesSufficient  = true;
+
+    bool isHabitable = isAtmosphereBreathable && isGravityStable;
+
+    Console.WriteLine(isHabitable);
+
+    bool suitableForExpansion = isGravityStable && resourcesSufficient;
+
+    Console.WriteLine(suitableForExpansion);
+
+    int currentCrew = 5;
+    int maxCrewCapacity = 10;
+
+    bool canAcceptMoreCrew = currentCrew < maxCrewCapacity;
+
+    Console.WriteLine(canAcceptMoreCrew);
+  }
+}
