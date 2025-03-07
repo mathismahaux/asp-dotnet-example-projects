a13d5d5bf6c081ab112c55403143646e3761b7e9
Mathis <mathis.mahaux@gmail.com>
2025-03-07 14:09:00 +0100

    Add Computron

---
+++ projects/chap2_intermediate/Computron/TodoList.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace SavingInterface
+{
+  class TodoList : IDisplayable, IResetable
+  {
+    public string[] Todos
+    { get; private set; }
+
+    private int nextOpenIndex;
+
+    public TodoList()
+    {
+      Todos = new string[5];
+      nextOpenIndex = 0;
+    }
+
+    public void Add(string todo)
+    {
+      Todos[nextOpenIndex] = todo;
+      nextOpenIndex++;
+    }
+
+    public void Display()
+    {
+      foreach (string todo in Todos)
+      {
+        if (!string.IsNullOrEmpty(todo))
+        {
+          Console.WriteLine(todo);
+        }
+      }
+    }
+
+    public void Reset()
+    {
+      Todos = new string[5];
+      nextOpenIndex = 0;
+    }
+  }
+}
